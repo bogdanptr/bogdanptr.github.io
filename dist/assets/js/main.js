0e0227ddfd69ccb8036b6311c983360626121a93
casadeconspiratii <bogdanes99@yahoo.com>
2017-11-09 23:28:48 +0200

    Added dist folder

---
+++ dist/assets/js/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".menu__toggle").click(function(){$(this).toggleClass("menu__toggle--active"),$(".extended-nav").toggleClass("extended-nav__active"),$("body").toggleClass("extended-nav__full")})});

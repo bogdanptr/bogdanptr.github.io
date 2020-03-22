804a092a1cf1bb7ab142dc08029e068941a5376c
Bogdan Petrescu <bogdan.petrescu@caphyon.com>
2020-03-22 21:50:05 +0200

    First commit with scroll revealing function

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -22,14 +22,41 @@ $.ajax({
     }
 });
 
+// Reveal element on scroll
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
-// Fluid typo
-// var textSpans = document.querySelectorAll('.project__details--title > span');
+function loop() {
 
-// for( var i=0; i<textSpans.length; i++ ){
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      }
+    });
 
-//  while( textSpans[i].offsetWidth > parseInt(window.getComputedStyle(textSpans[i].parentNode).width)  ){
-//    textSpans[i].style.fontSize = parseInt(window.getComputedStyle(textSpans[i]).fontSize) - 1 +"px";
-//  }
-  
-// }
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
